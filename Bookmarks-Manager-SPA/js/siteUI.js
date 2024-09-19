931d43c7e9ff16033885c4a248ec4c55dad61af5
MathieuDub01s <161617711+MathieuDub01s@users.noreply.github.com>
2024-09-18 20:24:26 -0400

    up

---
+++ Bookmarks-Manager-SPA/js/siteUI.js
@@ -0,0 +1,312 @@
+//<span class="cmdIcon fa-solid fa-ellipsis-vertical"></span>
+
+let contentScrollPosition = 0;
+let selectedCategory = "";
+Init_UI();
+
+function Init_UI() {
+    renderBookmarks();
+    $('#createBookmark').on("click", async function () {
+        saveContentScrollPosition();
+        renderCreateBookmarkForm();
+    });
+    $('#abort').on("click", async function () {
+        renderBookmarks();
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+}
+
+function renderAbout() {
+    saveContentScrollPosition();
+    eraseContent();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("À propos...");
+    $("#content").append(
+        $(`
+            <div class="aboutContainer">
+                <h2>Gestionnaire de favoris</h2>
+                <hr>
+                <p>
+                    Petite application de gestion de favoris
+                    d'interface utilisateur monopage réactive.
+                </p>
+                <p>
+                    Auteur: Mathieu Dubois
+                </p>
+                <p>
+                    Collège Lionel-Groulx, automne 2024
+                </p>
+            </div>
+        `))
+}
+async function renderBookmarks() {
+    showWaitingGif();
+    $("#actionTitle").text("Liste des favoris");
+    $("#createBookmark").show();
+    $("#abort").hide();
+    let bookmarks = await API_GetBookmarks();
+    eraseContent();
+    let arrayCategory = [];
+    if (bookmarks !== null) {
+        bookmarks.forEach(bookmark => {
+            arrayCategory.push(bookmark.Category);
+            if(selectedCategory == bookmark.Category){
+            $("#content").append(renderBookmark(bookmark));
+            }else if(selectedCategory == ""){
+                $("#content").append(renderBookmark(bookmark));
+            }
+        });
+        updateDropDownMenu([...new Set(arrayCategory)]);
+        restoreContentScrollPosition();
+        // Attached click events on command icons
+        $(".editCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderEditBookmarkForm(parseInt($(this).attr("editBookmarkId")));
+        });
+        $(".deleteCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderDeleteBookmarkForm(parseInt($(this).attr("deleteBookmarkId")));
+        });
+        $(".bookmarkRow").on("click", function (e) { e.preventDefault(); })
+    } else {
+        renderError("Service introuvable");
+    }
+}
+function showWaitingGif() {
+    $("#content").empty();
+    $("#content").append($("<div class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+}
+function eraseContent() {
+    $("#content").empty();
+}
+function saveContentScrollPosition() {
+    contentScrollPosition = $("#content")[0].scrollTop;
+}
+function restoreContentScrollPosition() {
+    $("#content")[0].scrollTop = contentScrollPosition;
+}
+function renderError(message) {
+    eraseContent();
+    $("#content").append(
+        $(`
+            <div class="errorContainer">
+                ${message}
+            </div>
+        `)
+    );
+}
+function renderCreateBookmarkForm() {
+    renderBookmarkForm();
+}
+async function renderEditBookmarkForm(id) {
+    showWaitingGif();
+    let bookmark = await API_GetBookmark(id);
+    if (bookmark !== null)
+        renderBookmarkForm(bookmark);
+    else
+        renderError("Favoris introuvable!");
+}
+async function renderDeleteBookmarkForm(id) {
+    showWaitingGif();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("Retrait");
+    let bookmark = await API_GetBookmark(id);
+    eraseContent();
+    if (bookmark !== null) {
+        $("#content").append(`
+        <div class="bookmarkdeleteForm">
+            <h4>Effacer le favoris suivant?</h4>
+            <br>
+            <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+              <div class="bookmarkContainerDelete">
+                <img class="bookmarkUrl" src="http://www.google.com/s2/favicons?sz=50&domain=${bookmark.Url}">
+                <span class="bookmarkTitle">${bookmark.Title}</span>
+                <br>
+                <span class="bookmarkCategory">${bookmark.Category}</span>
+                </div>  
+            </div>   
+            <br>
+            <input type="button" value="Effacer" id="deleteBookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </div>    
+        `);
+        $('#deleteBookmark').on("click", async function () {
+            showWaitingGif();
+            let result = await API_DeleteBookmark(bookmark.Id);
+            if (result)
+                renderBookmarks();
+            else
+                renderError("Une erreur est survenue!");
+        });
+        $('#cancel').on("click", function () {
+            renderBookmarks();
+        });
+    } else {
+        renderError("Favoris introuvable!");
+    }
+}
+function newBookmark() {
+    bookmark = {};
+    bookmark.Id = 0;
+    bookmark.Title = "";
+    bookmark.Url = "";
+    bookmark.Category = "";
+    return bookmark;
+}
+function showIcon(){
+    var inpuValue = document.getElementById("targetUrl").value;
+    const imgIcon = document.getElementById("siteIconTarget");
+    let imgIconsrc = "http://www.google.com/s2/favicons?sz=50&domain="+inpuValue;
+    imgIcon.setAttribute("src", imgIconsrc);
+    console.log(imgIconsrc);
+    if(inpuValue == null || inpuValue == ""){
+        imgIcon.setAttribute("src", "bookmark-star.svg");
+    }
+}
+
+function renderBookmarkForm(bookmark = null) {
+    $("#createBookmark").hide();
+    $("#abort").show();
+    eraseContent();
+    let changeIcon = "";
+    let create = bookmark == null;
+    if (create) bookmark = newBookmark();
+    if(bookmark.Url != null || bookmark.Url != "")
+        changeIcon = `http://www.google.com/s2/favicons?sz=50&domain=${bookmark.Url}`;
+    if(bookmark.Url == ""){
+        changeIcon = "bookmark-star.svg"
+    }
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#content").append(`
+        <form class="form" id="bookmarkForm" name="createBookmarkForm">
+           <img id="siteIconTarget" class="bookmarkUrlN" src="${changeIcon}">
+           <br>
+            <input type="hidden" name="Id" value="${bookmark.Id}"/>
+
+            <label for="Name" class="form-label">Titre </label>
+            <input 
+                class="form-control Alpha"
+                name="Title" 
+                id="Title" 
+                placeholder="Titre"
+                required
+                RequireMessage="Veuillez entrer un titre"
+                InvalidMessage="Le nom comporte un caractère illégal" 
+                value="${bookmark.Title}"
+            />
+            <label for="Phone" class="form-label">Url </label>
+            <input
+                onkeyup="showIcon()"
+                class="form-control Url"
+                name="Url"
+                id="targetUrl"
+                placeholder="Url"
+                required
+                RequireMessage="Veuillez entrer votre téléphone" 
+                InvalidMessage="Veuillez entrer un téléphone valide"
+                value="${bookmark.Url}" 
+            />
+            <label for="Email" class="form-label">Catégorie </label>
+            <input 
+                class="form-control Alpha"
+                name="Category"
+                id="Category"
+                placeholder="Catégorie"
+                required
+                RequireMessage="Veuillez entrer votre courriel" 
+                InvalidMessage="Veuillez entrer un courriel valide"
+                value="${bookmark.Category}"
+            />
+            <hr>
+            <input type="submit" value="Enregistrer" id="saveBookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    initFormValidation();
+    $('#bookmarkForm').on("submit", async function (event) {
+        event.preventDefault();
+        let bookmark = getFormData($("#bookmarkForm"));
+        bookmark.Id = parseInt(bookmark.Id);
+        showWaitingGif();
+        let result = await API_SaveBookmark(bookmark, create);
+        if (result)
+            renderBookmarks();
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        renderBookmarks();
+    });
+}
+
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+
+function renderBookmark(bookmark) {
+    return $(`
+     <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+        <div class="bookmarkContainer noselect">
+            <div class="contactLayout">
+                <img class="bookmarkUrl" src="http://www.google.com/s2/favicons?sz=50&domain=${bookmark.Url}">
+                <span class="bookmarkTitle">${bookmark.Title}</span>
+                <br>
+                <span class="bookmarkCategory">${bookmark.Category}</span>
+            </div>
+            <div class="bookmarkCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editBookmarkId="${bookmark.Id}" title="Modifier ${bookmark.Title}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deleteBookmarkId="${bookmark.Id}" title="Effacer ${bookmark.Title}"></span>
+            </div>
+        </div>
+    </div>           
+    `);
+}
+
+
+/////////////////////////////////
+
+function updateDropDownMenu(categories) {
+let DDMenu = $("#DDMenu");
+let selectClass = selectedCategory === "" ? "fa-check" : "fa-fw";
+DDMenu.empty();
+DDMenu.append($(`
+<div class="dropdown-item menuItemLayout" id="allCatCmd">
+<i class="menuIcon fa ${selectClass} mx-2"></i> Toutes les catégories
+</div>
+`));
+DDMenu.append($(`<div class="dropdown-divider"></div>`));
+categories.forEach(category => {
+selectClass = selectedCategory === category ? "fa-check" : "fa-fw";
+DDMenu.append($(`
+<div class="dropdown-item menuItemLayout category" id="allCatCmd">
+<i class="menuIcon fa ${selectClass} mx-2"></i> ${category}
+</div>
+`));
+})
+DDMenu.append($(`<div class="dropdown-divider"></div> `));
+DDMenu.append($(`
+<div class="dropdown-item menuItemLayout" id="aboutCmd">
+<i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+</div>
+`));
+$('#aboutCmd').on("click", function () {
+renderAbout();
+});
+$('#allCatCmd').on("click", function () {
+selectedCategory = "";
+renderBookmarks();
+});
+$('.category').on("click", function () {
+selectedCategory = $(this).text().trim();
+renderBookmarks();
+});
+}
